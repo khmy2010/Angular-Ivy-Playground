38220accfdce2f296efacd811a33e4f95cb64459
khmy2010 <kehan.see@gmail.com>
2019-07-20 21:35:19 +0800

    test: 💍 todo test

--- cypress/integration/todos_spec.js
+++ cypress/integration/todos_spec.js
@@ -8,11 +8,21 @@ describe("Todos", () => {
 
   it('should shows default message', () => {
     cy.get('.todo-prompt').should('have.length.greaterThan', 0);
+    cy.get('#todo-pending .title').should('have.text', '0');
+    cy.get('#todo-completed .title').should('have.text', '0');
+    cy.get('#todo-all .title').should('have.text', '0');
   })
 
   it('should change state once \'add new\' is clicked', () => {
     cy.contains('adding one').click();
 
     cy.get('.box > .button').should('have.length.greaterThan', 0);
+    cy.get('#todo-pending .title').should('have.text', '1');
+    cy.get('#todo-completed .title').should('have.text', '0');
+    cy.get('#todo-all .title').should('have.text', '1');
+
+    cy.get('.todo input').first().should('have.attr', 'placeholder', 'Buy Durian at SS2');
+
+    cy.get('#deleteCount').should('have.text', 'Delete All (1)');
   })
 });
