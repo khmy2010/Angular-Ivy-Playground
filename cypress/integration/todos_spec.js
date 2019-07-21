e574cbc6617e422e51c9886550049bb921eacb79
khmy2010 <kehan.see@gmail.com>
2019-07-21 17:26:19 +0800

    test: 💍 added more tests for todo

--- cypress/integration/todos_spec.js
+++ cypress/integration/todos_spec.js
@@ -66,4 +66,63 @@ describe('Manipulating One Todo', () => {
     cy.contains(month).should('have.length.greaterThan', 0);
     cy.contains(day).should('have.length.greaterThan', 0);
   });
+
+  it('should be deleted once a \'delete all\' is pressed.', () => {
+    cy.get('#deleteCount').click();
+    cy.get('.todo').should('have.length', 0);
+  })
 })
+
+describe('Todo Counts', () => {
+  beforeEach(() => {
+    cy.visit("/todos");
+
+    const VALUE = 'read a book';
+
+    cy.contains('adding one').click();
+
+    cy.get('.todo input')
+      .type(VALUE).should('have.value', VALUE);
+
+    cy.get('#addNewTodo').click();
+
+    const SECOND_VALUE = 'eat nasi lemak';
+    cy.get('.todo').find('input').eq(1).type(SECOND_VALUE).should('have.value', SECOND_VALUE);
+
+
+    //aliases
+    cy.get('#todo-pending > div > .title').as('pending-count');
+    cy.get('#todo-completed > div > .title').as('completed-count');
+    cy.get('#todo-all > div > .title').as('all-count');
+  })
+
+  it('should have (2) in pending', () => {
+    cy.get('@pending-count').should('have.text', '2');
+  })
+
+  it('should have (0) in completed', () => {
+    cy.get('@completed-count').should('have.text', '0');
+  })
+
+  it('should have (2) in all', () => {
+    cy.get('@all-count').should('have.text', '2');
+  })
+
+  it('should be updated accordingly when a todo is completed', () => {
+    cy.get('.todo').eq(1).contains('Done').click();
+    cy.contains('eat nasi lemak').parents().should('have.class', 'completed-todo');
+    cy.get('@pending-count').should('have.text', '1');
+    cy.get('@completed-count').should('have.text', '1');
+    cy.get('@all-count').should('have.text', '2');
+  })
+
+  it('should be updated accordingly when all todo is completed', () => {
+    cy.get('.todo').each(($ele) => {
+      cy.wrap($ele).contains('Done').click();
+    })
+
+    cy.get('@pending-count').should('have.text', '0');
+    cy.get('@completed-count').should('have.text', '2');
+    cy.get('@all-count').should('have.text', '2');
+  });
+});
